665fba1ea62b9b4988ffacafa43c97b9971b05cd
DmitryeMaluckow <dim.maluckow2017@yandex.ru>
2026-02-16 14:14:14 +0300

    1.1.1 Writing Markup with JSX

--- src/pages/main/1-main-page/movies/1-movies/movie.tsx
+++ src/pages/main/1-main-page/movies/1-movies/movie.tsx
@@ -1,8 +1,17 @@
 export const Movie = () => {
-  return <div>
-    <h4>Интерстеллар</h4>
-     2014
-    <img src="https://image.openmoviedb.com/kinopoisk-images/1600647/430042eb-ee69-4818-aed0-a312400a26bf/x1000" alt = "Интерстеллар (2014)" style={{width: "300px"}}></img>
-    <div>Фантастический эпос про задыхающуюся Землю, космические полеты и парадоксы времени. «Оскар» за спецэффекты</div>
-  </div>;
+  return (
+    <div>
+      <h4>Интерстеллар</h4>
+      2014
+      <img
+        src="https://image.openmoviedb.com/kinopoisk-images/1600647/430042eb-ee69-4818-aed0-a312400a26bf/x1000"
+        alt="Интерстеллар (2014)"
+        style={{ width: '300px' }}
+      ></img>
+      <div>
+        Фантастический эпос про задыхающуюся Землю, космические полеты и
+        парадоксы времени. «Оскар» за спецэффекты
+      </div>
+    </div>
+  );
 };
