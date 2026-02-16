8280fe1f7502e006fce89689f1385a75b5e8ac3c
DmitryeMaluckow <dim.maluckow2017@yandex.ru>
2026-02-16 16:36:50 +0300

    1.2.1 Passing Props to a Component | Conditional Rendering

--- src/pages/main/1-main-page/movies/2-movies/movie.tsx
+++ src/pages/main/1-main-page/movies/2-movies/movie.tsx
@@ -1,18 +1,20 @@
-import { MovieType } from "./types";
+import type { MovieType } from './types';
 
 type Props = {
-  movie: MovieType,
-  isFavorite: boolean
-}
+  movie: MovieType;
+  isFavorite: boolean;
+};
 
-export const Movie = ({movie, isFavorite}: Props) => {
-  return <div className="max-w-72">
-    <h4>{movie.title}{isFavorite && "\u2B50"}</h4>
+export const Movie = ({ movie, isFavorite }: Props) => {
+  return (
+    <div className="max-w-72">
+      <h4>
+        {movie.title}
+        {isFavorite && '\u2B50'}
+      </h4>
       {movie.year}
-      <img
-        src={movie.posterUrl}
-        alt={`${movie.title} (${movie.year})`}
-      ></img>
+      <img src={movie.posterUrl} alt={`${movie.title} (${movie.year})`}></img>
       <div className="text-base">{movie.description}</div>
-  </div>;
+    </div>
+  );
 };
--- src/pages/main/1-main-page/movies/2-movies/movies.tsx
+++ src/pages/main/1-main-page/movies/2-movies/movies.tsx
@@ -1,14 +1,16 @@
-import { favorites } from "./favorites";
-import { getFirstMovie, getSecondMovie, getThirdMovie } from "./getters";
-import { Movie } from "./movie";
+import { favorites } from './favorites';
+import { getFirstMovie, getSecondMovie, getThirdMovie } from './getters';
+import { Movie } from './movie';
 
 export const Movies = () => {
   const firstMovie = getFirstMovie();
   const secondMovie = getSecondMovie();
   const thirdMovie = getThirdMovie();
-  return <div className="flex space-x-5">
-    <Movie movie={firstMovie} isFavorite = {favorites.has(firstMovie.id)}/>
-    <Movie movie={secondMovie} isFavorite = {favorites.has(secondMovie.id)}/>
-    <Movie movie={thirdMovie} isFavorite = {favorites.has(thirdMovie.id)}/>
-  </div>;
+  return (
+    <div className="flex space-x-5">
+      <Movie movie={firstMovie} isFavorite={favorites.has(firstMovie.id)} />
+      <Movie movie={secondMovie} isFavorite={favorites.has(secondMovie.id)} />
+      <Movie movie={thirdMovie} isFavorite={favorites.has(thirdMovie.id)} />
+    </div>
+  );
 };
