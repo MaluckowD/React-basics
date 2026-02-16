11900444a5b380c3384e08c79bfa1371e0c7902d
DmitryeMaluckow <dim.maluckow2017@yandex.ru>
2026-02-16 16:49:16 +0300

    1.2.2 Rendering Lists

--- src/pages/main/1-main-page/movies/3-movies/movies.tsx
+++ src/pages/main/1-main-page/movies/3-movies/movies.tsx
@@ -1,14 +1,20 @@
-import { getMovies } from "./get-movies";
-import { Movie } from "./movie";
 import { favorites } from './favorites';
+import { getMovies } from './get-movies';
+import { Movie } from './movie';
 
 export const Movies = () => {
   const movies = getMovies();
-  return <div className="flex space-x-5">
-    {movies.map( (item) => {
-      return (
-        <Movie key = {item.id} movie={item} isFavorite={favorites.has(item.id)} />
-      )
-    })}
-  </div>;
+  return (
+    <div className="flex space-x-5">
+      {movies.map((item) => {
+        return (
+          <Movie
+            key={item.id}
+            movie={item}
+            isFavorite={favorites.has(item.id)}
+          />
+        );
+      })}
+    </div>
+  );
 };
