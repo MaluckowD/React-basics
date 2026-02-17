b8c0e0c44ef5b6e4d71b2e9ba4f46b3ba865671c
DmitryeMaluckow <dim.maluckow2017@yandex.ru>
2026-02-17 13:09:52 +0300

    1.3 Understanding Your UI as a Tree

--- src/pages/main/1-main-page/add-movie-form/add-movie-form.tsx
+++ src/pages/main/1-main-page/add-movie-form/add-movie-form.tsx
@@ -1,14 +1,20 @@
-import { Button } from "@/components";
-import { DescriptionField, PosterUrlField, TitleField, YearField } from "./form-fields";
+import { Button } from '@/components';
+
+import {
+  DescriptionField,
+  PosterUrlField,
+  TitleField,
+  YearField,
+} from './form-fields';
 
 export const AddMovieForm = () => {
   return (
-    <form aria-label = "Форма добавления фильма" className="max-w-sm my-5">
-      <TitleField/>
-      <YearField/>
-      <PosterUrlField/>
-      <DescriptionField/>
+    <form aria-label="Форма добавления фильма" className="max-w-sm my-5">
+      <TitleField />
+      <YearField />
+      <PosterUrlField />
+      <DescriptionField />
       <Button>Добавить</Button>
     </form>
-  )
+  );
 };
--- src/pages/main/1-main-page/add-movie-form/form-fields/title-field.tsx
+++ src/pages/main/1-main-page/add-movie-form/form-fields/title-field.tsx
@@ -1,9 +1,19 @@
 export const TitleField = () => {
   return (
     <div className="mb-5">
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="title">Название фильма</label>
-      <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-      id = "title" type = "text" placeholder = "Введите название фильма"/>
+      <label
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        htmlFor="title"
+      >
+        Название фильма
+      </label>
+      <input
+        className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        required
+        id="title"
+        type="text"
+        placeholder="Введите название фильма"
+      />
     </div>
-  )
+  );
 };
